cec45dd73caa9e45a965edf3d94dd063edb26a06
Matthew Pflueger <matthew.pflueger@gmail.com>
2011-11-07 15:37:49 -0500

    Fixed error inserting RetailerConfirmation

--- src/main/scala/com/echoed/chamber/dao/RetailerConfirmationDao.scala
+++ src/main/scala/com/echoed/chamber/dao/RetailerConfirmationDao.scala
@@ -5,7 +5,7 @@ import com.echoed.chamber.domain.RetailerConfirmation
 
 trait RetailerConfirmationDao {
 
-    def insertRetailerConfirmation(retailerConfirmation: RetailerConfirmation): RetailerConfirmation
+    def insertRetailerConfirmation(retailerConfirmation: RetailerConfirmation): Int
 
     def selectRetailerConfirmationCount: Long
 
