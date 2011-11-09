7e61666bbebe48be0f9b56ccbc4e6157293846e2
Matthew Pflueger <matthew.pflueger@gmail.com>
2011-11-09 18:35:22 -0500

    Added some unit tests for EchoPossibility and FacebookAccessActor

---
+++ src/test/scala/com/echoed/chamber/domain/EchoPossibilityTest.scala
@@ -0,0 +1,49 @@
+package com.echoed.chamber.domain
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{GivenWhenThen, Spec}
+import org.scalatest.matchers.ShouldMatchers
+import java.util.Date
+
+
+@RunWith(classOf[JUnitRunner])
+class EchoPossibilityTest extends Spec with GivenWhenThen with ShouldMatchers {
+
+    describe("An EchoPossibility") {
+
+        it("should return null for the id if missing properties") {
+            given("a newly instantiated EchoPossibility")
+            val echoPossibility = new EchoPossibility
+
+            when("no properties have been set")
+            then("the id should be null")
+            echoPossibility.id should be (null)
+        }
+
+        it("should return a url safe base 64 string when all necessary properties are set") {
+            given("an EchoPossibility with all necessary properties set")
+            val (echoPossibility, expectedBase64Value) = EchoPossibilityHelper.getValidEchoPossibilityAndHash
+
+            when("all necessary properties have been set")
+            then("the id should return a valid base64 string")
+            echoPossibility.id should equal (expectedBase64Value)
+        }
+    }
+}
+
+object EchoPossibilityHelper {
+    def getValidEchoPossibilityAndHash = {
+        (new EchoPossibility(
+                "testRetailerId",
+                "testRetailerCustomerId",
+                "testProductId",
+                new Date(1320871016126L), //Wed Nov 09 15:36:56 EST 2011
+                null,
+                null
+                ),
+
+        //a normal base64 will have one or more '=' characters for padding - they are ripped off for url safe base64 strings...
+        "dGVzdFJldGFpbGVySWR0ZXN0UmV0YWlsZXJDdXN0b21lcklkdGVzdFByb2R1Y3RJZFdlZCBOb3YgMDkgMTU6MzY6NTYgRVNUIDIwMTE")
+    }
+}
---
+++ src/test/scala/com/echoed/chamber/services/facebook/FacebookAccessActorTest.scala
@@ -0,0 +1,29 @@
+package com.echoed.chamber.services.facebook
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.{Spec, GivenWhenThen}
+import java.util.Properties
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import akka.testkit.TestActorRef
+
+@RunWith(classOf[JUnitRunner])
+class FacebookAccessActorTest extends Spec with GivenWhenThen with ShouldMatchers {
+
+    describe("A FacebookAccessActor") {
+
+        it("should not start if missing properties") {
+            given("an empty Properties object set on a FacebookAccessActor")
+            val actorRef = TestActorRef[FacebookAccessActor]
+            actorRef.underlyingActor.properties = new Properties
+
+            when("the actor is started")
+            then("the actor should not start")
+            and("an AssertionError will have been thrown")
+            evaluating {
+                actorRef.start
+            } should produce [AssertionError]
+            actorRef.isUnstarted should be (true)
+        }
+    }
+}
