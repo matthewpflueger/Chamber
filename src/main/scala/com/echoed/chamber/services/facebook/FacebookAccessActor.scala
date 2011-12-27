41a750978c9fc3b9cd2c623c1c4ad9ee5594f758
Matthew Pflueger <matthew.pflueger@gmail.com>
2011-12-27 15:50:25 -0500

    Normalized design - put Me into FacebookAccessActor with the rest

--- src/main/scala/com/echoed/chamber/services/facebook/FacebookAccessActor.scala
+++ src/main/scala/com/echoed/chamber/services/facebook/FacebookAccessActor.scala
@@ -213,6 +213,42 @@ case class Friend(
         name)
 }
 
+class Me() {
+
+    @BeanProperty var id: String = null
+    @BeanProperty var name: String = null
+    @BeanProperty var first_name: String = null
+    @BeanProperty var middle_name: String = null
+    @BeanProperty var last_name: String = null
+    @BeanProperty var link: String = null
+    @BeanProperty var gender: String = null
+    @BeanProperty var email: String = null
+    @BeanProperty var timezone: String = null
+    @BeanProperty var locale: String = null
+    @BeanProperty var verified: String = null
+    @BeanProperty var updated_time: String = null
+
+    def createFacebookUser(accessToken: String) = new FacebookUser(
+        null,
+        id,
+        name,
+        email,
+        link,
+        gender,
+        timezone,
+        locale,
+        accessToken
+    )
+
+    def createFacebookTestUser(loginUrl: String, accessToken: String, password: String = "1234567890") = new FacebookTestUser(
+        id,
+        name,
+        email,
+        password,
+        loginUrl,
+        accessToken)
+}
+
 
 
 
