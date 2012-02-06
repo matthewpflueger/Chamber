87edcbd57596eeb8a94b6869194bfcb6982f23db
Jonathan Wu <JonLWu@gmail.com>
2012-02-06 14:57:53 -0500

    Fixed lb_sample.png to be generic

---
+++ src/main/scala/com/echoed/chamber/dao/views/AdminViewDao.scala
@@ -0,0 +1,13 @@
+package com.echoed.chamber.dao.views
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jonlwu
+ * Date: 2/6/12
+ * Time: 10:34 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class AdminViewDao {
+
+}
---
+++ src/main/scala/com/echoed/chamber/domain/AdminUser.scala
@@ -0,0 +1,13 @@
+package com.echoed.chamber.domain
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jonlwu
+ * Date: 2/6/12
+ * Time: 10:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class AdminUser {
+
+}
