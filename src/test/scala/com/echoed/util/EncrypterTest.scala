8d45d9fd9d7e07f8b92d48c340f984d6abbff411
Matthew Pflueger <matthew.pflueger@gmail.com>
2012-02-16 11:21:38 -0500

    Added test to match PHP AES encryption

--- src/test/scala/com/echoed/util/EncrypterTest.scala
+++ src/test/scala/com/echoed/util/EncrypterTest.scala
@@ -5,10 +5,7 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.{GivenWhenThen, Spec}
 import org.scalatest.matchers.ShouldMatchers
 import org.slf4j.LoggerFactory
-import org.apache.commons.codec.binary.Base64
-import javax.crypto.Cipher
-import javax.crypto.spec.{IvParameterSpec, SecretKeySpec}
-import com.echoed.util.Encrypter
+import org.apache.commons.codec.binary.{Hex, Base64}
 
 
 @RunWith(classOf[JUnitRunner])
@@ -16,22 +13,57 @@ class EncrypterTest extends Spec with GivenWhenThen with ShouldMatchers {
 
     private val logger = LoggerFactory.getLogger(classOf[EncrypterTest])
 
+    val text = "The quick brown fox jumped over the lazy dog"
+
+    val encrypter = new Encrypter()
+    encrypter.init()
+
+    var secret: String = null
+    var cipherText: String = null
+
     describe("Encrypter") {
 
-        it("should encrypt/decrypt with a generated secret key") {
+        it("should generate a generated secret key") {
             given("a properly initialized Encrypter")
-            when("asked to generate a secret and then encrypt/decrypt")
-            then("it should do so without errors")
+            when("asked to generate a secret")
+            then("it should return a generated unique secret key")
 
-            val encrypter = new Encrypter()
-            encrypter.init()
+            secret = encrypter.generateSecretKey
+            secret should not be(null)
+        }
 
-            val text = "The quick brown fox jumped over the lazy dog"
+        it("should encrypt with a secret key") {
+            given("a properly initialized Encrypter")
+            when("asked to encrypt text")
+            then("it should return an encrypted string")
 
-            val secret = encrypter.generateSecretKey
-            val cipherText = encrypter.encrypt(text, secret)
+            secret should not be(null)
+            cipherText = encrypter.encrypt(text, secret)
+            cipherText should not be(null)
             text should equal(encrypter.decrypt(cipherText, secret))
         }
 
+        it("should decrypt with a secret key") {
+            given("a properly initialized Encrypter")
+            when("asked to decrypt text")
+            then("it should return a decrypted string")
+
+            secret should not be(null)
+            cipherText should not be(null)
+            text should equal(encrypter.decrypt(cipherText, secret))
+        }
+
+        it("should match php AES encryption") {
+            given("a properly initialized Encrypter")
+            when("asked to encrypt/decrypt some text")
+            then("it should match typical php AES encryption of the same string")
+
+            val s = Base64.encodeBase64URLSafeString("1234567890123456".getBytes("US-ASCII"))
+            val cT = encrypter.encrypt(text, s)
+
+            val hCT = Hex.encodeHexString(Base64.decodeBase64(cT))
+            //see http://www.chilkatsoft.com/p/php_aes.asp
+            hCT should equal("f78176ae8dfe84578529208d30f446bbb29a64dc388b5c0b63140a4f316b3f341fe7d3b1a3cc5113c81ef8dd714a1c99")
+        }
     }
 }
