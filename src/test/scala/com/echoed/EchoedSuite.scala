2ca9bcc3794b19cfec984542744474bb4b02963c
Matthew Pflueger <matthew.pflueger@gmail.com>
2012-02-16 16:16:27 -0500

    Added EncrypterTest to EchoedSuite

--- src/test/scala/com/echoed/EchoedSuite.scala
+++ src/test/scala/com/echoed/EchoedSuite.scala
@@ -4,9 +4,11 @@ import org.scalatest.Suites
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import com.echoed.chamber.{ChamberITSuite, ChamberTestSuite}
+import com.echoed.util.EncrypterTest
 
 @RunWith(classOf[JUnitRunner])
 class EchoedSuite extends Suites(
+    new EncrypterTest,
     new ChamberTestSuite,
     new ChamberITSuite)
 
