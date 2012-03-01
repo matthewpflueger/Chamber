a234ff46c6e6584c5e12fe21cb54762b156793ad
Matthew Pflueger <matthew.pflueger@gmail.com>
2012-03-01 18:34:28 -0500

    Added better logging configuration - now have error and debug log

--- src/main/scala/com/echoed/chamber/Main.scala
+++ src/main/scala/com/echoed/chamber/Main.scala
@@ -1,13 +1,21 @@
 package com.echoed.chamber
 
 import org.springframework.context.support.FileSystemXmlApplicationContext
+import org.slf4j.LoggerFactory
+import ch.qos.logback.core.util.StatusPrinter
+import ch.qos.logback.classic.LoggerContext
 
 
 object Main {
 
+    private val logger = LoggerFactory.getLogger(classOf[Main])
+
     def main(args: Array[String]) {
+        logger.debug("Loading Spring context {}", args(0))
         val ctx = new FileSystemXmlApplicationContext(args(0))
         ctx.registerShutdownHook
     }
 
 }
+
+class Main {}
