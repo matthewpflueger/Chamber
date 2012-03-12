9d6c3a0896deb7ddb06f930d166ef742903f9893
Matthew Pflueger <matthew.pflueger@gmail.com>
2012-03-12 10:08:14 -0400

    Made deserialization of JSON more flexible to fix unknown description field on FacebookPost

--- src/main/scala/com/echoed/util/ScalaObjectMapper.scala
+++ src/main/scala/com/echoed/util/ScalaObjectMapper.scala
@@ -1,9 +1,13 @@
 package com.echoed.util
 
-import org.codehaus.jackson.map.ObjectMapper
 import com.codahale.jerkson.ScalaModule
+import org.codehaus.jackson.map.{DeserializationConfig, ObjectMapper}
 
 
 class ScalaObjectMapper extends ObjectMapper {
     registerModule(new ScalaModule(Thread.currentThread().getContextClassLoader))
+    setDeserializationConfig(getDeserializationConfig
+            .without(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .`with`(DeserializationConfig.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+            .without(DeserializationConfig.Feature.FAIL_ON_NULL_FOR_PRIMITIVES))
 }
