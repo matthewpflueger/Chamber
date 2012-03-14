24f83373b9f8d7fbb88c3839dbdb107696a660a9
Matthew Pflueger <matthew.pflueger@gmail.com>
2012-03-14 14:15:01 -0400

    Added directory for collecting useful sql statements/scripts for reference

---
+++ src/main/database/reference/facebookPostCommentsAndLikes.sql
@@ -0,0 +1,13 @@
+select
+    EU.name,
+    FP.id,
+    FP.crawledOn,
+    FP.crawledStatus,
+    FC.name as commentBy,
+    FC.message,
+    FL.name as likedBy
+from FacebookPost FP
+    join EchoedUser EU on EU.id = FP.echoedUserId
+    join FacebookComment FC on FC.echoedUserId = EU.id
+    left join FacebookLike FL on FL.echoedUserId = EU.id
+where EU.name like '%Vanes%';
