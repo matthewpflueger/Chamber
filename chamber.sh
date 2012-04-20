ebb83c55496986ccd3182c3ec6b066157a3c6895
Matthew Pflueger <matthew.pflueger@gmail.com>
2012-04-19 22:56:18 -0400

    Added database specific dumping

--- chamber.sh
+++ chamber.sh
@@ -11,262 +11,263 @@ if [[ ! -e "$JAVA_HOME/bin/java" ]]; then
     echo "Setting JAVA_HOME=${JAVA_HOME}"
 fi
 
+if [[ "${ENV_TYPE}" == "" ]]; then
+    ENV_TYPE=dev
+fi
+
+CONTEXT=src/main/resources/jetty.xml
+TARGET=target/chamber-0.1-SNAPSHOT-allinone.jar
+MAIN=com.echoed.chamber.Main
+
+NEWRELIC=/usr/local/lib/newrelic/newrelic.jar
+
+MIN_MEM="1024m"
+MAX_MEM="2048m"
+PERM_SIZE="256m"
+CONNECT_TIMEOUT="10000"
+READ_TIMEOUT="10000"
+CLASSPATH=".:${TARGET}"
+OVERRIDES="src/overrides/resources"
+ARGS_INTERESTING="-XX:+CMSPermGenSweepingEnabled -XX:+CMSClassUnloadingEnabled -XX:+UseCompressedOops"
+
+DATABASE=Echoed
+DATABASE_USER=root
+DATABASE_PASSWORD=password
+
+# Load the system environment variable overrides
+[ -r /etc/default/$NAME ] && . /etc/default/$NAME
+[ -r $BASEDIR/$NAME ] && . $BASEDIR/$NAME
+
+
 function service_cmd() {
 
-local service_args=($*)
+    local service_args=($*)
 
-case $service_args in
-    clean)
-        CLEAN="mvn -DskipTests clean"
-        echo "Running ${CLEAN}"
-        $CLEAN
-        result=$?
-        if [[ $result > 0 ]]; then
-            exit 1;
-        fi
-        ;;
+    case $service_args in
+        clean)
+            CLEAN="mvn -DskipTests clean"
+            echo "Running ${CLEAN}"
+            $CLEAN
+            result=$?
+            if [[ $result > 0 ]]; then
+                exit 1;
+            fi
+            ;;
 
-    package)
-        PACKAGE="mvn -DskipTests -Pallinone package"
-        echo "Running ${PACKAGE}"
-        $PACKAGE
-        result=$?
-        if [[ $result > 0 ]]; then
-            exit 1;
-        fi
-        ;;
+        package)
+            PACKAGE="mvn -DskipTests -Pallinone package"
+            echo "Running ${PACKAGE}"
+            $PACKAGE
+            result=$?
+            if [[ $result > 0 ]]; then
+                exit 1;
+            fi
+            ;;
 
-    start)
-        echo "Starting $DESC"
-        shift
-
-        if [[ "${ENV_TYPE}" == "" ]]; then
-            ENV_TYPE=dev
-        fi
-
-        CONTEXT=src/main/resources/jetty.xml
-        TARGET=target/chamber-0.1-SNAPSHOT-allinone.jar
-        MAIN=com.echoed.chamber.Main
-
-        NEWRELIC=/usr/local/lib/newrelic/newrelic.jar
-
-        MIN_MEM="1024m"
-        MAX_MEM="2048m"
-        PERM_SIZE="256m"
-        CONNECT_TIMEOUT="10000"
-        READ_TIMEOUT="10000"
-        CLASSPATH=".:${TARGET}"
-        OVERRIDES="src/overrides/resources"
-        ARGS_INTERESTING="-XX:+CMSPermGenSweepingEnabled -XX:+CMSClassUnloadingEnabled -XX:+UseCompressedOops"
-
-
-        # Load the system environment variable overrides
-        [ -r /etc/default/$NAME ] && . /etc/default/$NAME
-        [ -r $BASEDIR/$NAME ] && . $BASEDIR/$NAME
-        #if [ -e /etc/default/$NAME ]; then 
-        #    source /etc/default/$NAME
-        #    echo "ENV_TYPE=${ENV_TYPE}"
-        #fi
-
-        ARGS="-server -Xms${MIN_MEM} -Xmx${MAX_MEM} -XX:PermSize=${PERM_SIZE} -Djava.net.preferIPv4Stack=true -Dsun.net.client.defaultConnectTimeout=${CONNECT_TIMEOUT} -Dsun.net.client.defaultReadTimeout=${READ_TIMEOUT}"
-
-
-        if [[ "$1" == "-o" ]]; then
-            CLASSPATH="${OVERRIDES}:${CLASSPATH}"
+        start)
+            echo "Starting $DESC"
             shift
-        fi
 
-        if [[ "$1" == "-d" ]]; then
-            ARGS="$ARGS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000"
+
+            ARGS="-server -Xms${MIN_MEM} -Xmx${MAX_MEM} -XX:PermSize=${PERM_SIZE} -Djava.net.preferIPv4Stack=true -Dsun.net.client.defaultConnectTimeout=${CONNECT_TIMEOUT} -Dsun.net.client.defaultReadTimeout=${READ_TIMEOUT}"
+
+
+            if [[ "$1" == "-o" ]]; then
+                CLASSPATH="${OVERRIDES}:${CLASSPATH}"
+                shift
+            fi
+
+            if [[ "$1" == "-d" ]]; then
+                ARGS="$ARGS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=8000"
+                shift
+            fi
+
+            if [ -e ${NEWRELIC} ]; then
+                ARGS="-javaagent:${NEWRELIC} $ARGS"
+            fi
+
+            if [ ! -e ${TARGET} ]; then
+                echo "Missing ${TARGET}"
+                service_cmd "package"
+            fi
+
+            if [ ! ${1} ]; then
+                echo "Using default Spring context configuration ${CONTEXT}"
+            else
+                CONTEXT=${1}
+            fi
+
+
+            echo "java ${ARGS} -DENV_TYPE=${ENV_TYPE} -cp ${CLASSPATH} ${MAIN} ${CONTEXT}"
+
+
+            # We do this to capture the pid of the process
+            sh -c "java ${ARGS} -DENV_TYPE=${ENV_TYPE} -cp ${CLASSPATH} ${MAIN} ${CONTEXT} >./std.out 2>&1 & APID=\"\$!\"; echo \$APID > chamber.pid"
+            ;;
+
+        startt)
+            service_cmd "start"
+            tail -f std.out
+            ;;
+
+        stop)
             shift
-        fi
+            echo "Stopping $DESC"
+            kill -9 `cat chamber.pid`
+            ;;
 
-        if [ -e ${NEWRELIC} ]; then
-            ARGS="-javaagent:${NEWRELIC} $ARGS"
-        fi
+        restart)
+            service_cmd "stop"
+            service_cmd "start"
+            ;;
 
-        if [ ! -e ${TARGET} ]; then
-            echo "Missing ${TARGET}"
+        reload)
+            service_cmd "dump"
+            service_cmd "clean"
             service_cmd "package"
-        fi
+            service_cmd "restart"
+            ;;
 
-        if [ ! ${1} ]; then
-            echo "Using default Spring context configuration ${CONTEXT}"
-        else
-            CONTEXT=${1}
-        fi
-
-
-        echo "java ${ARGS} -DENV_TYPE=${ENV_TYPE} -cp ${CLASSPATH} ${MAIN} ${CONTEXT}"
-
-
-        # We do this to capture the pid of the process
-        sh -c "java ${ARGS} -DENV_TYPE=${ENV_TYPE} -cp ${CLASSPATH} ${MAIN} ${CONTEXT} >./std.out 2>&1 & APID=\"\$!\"; echo \$APID > chamber.pid"
-        ;;
-
-    startt)
-        service_cmd "start"
-        tail -f std.out
-        ;;
-
-    stop)
-        shift
-        echo "Stopping $DESC"
-        kill -9 `cat chamber.pid`
-        ;;
-
-    restart)
-        service_cmd "stop"
-        service_cmd "start"
-        ;;
-
-    reload)
-        service_cmd "dump"
-        service_cmd "clean"
-        service_cmd "package"
-        service_cmd "restart"
-        ;;
-
-    verify)
-        shift
-        echo "Running integration tests for $DESC"
-        displaycmd=""
-        if [[ "$1" == "-d" ]]; then
-            displyacmd="-DdisplayCmd=true"
+        verify)
             shift
-        fi
+            echo "Running integration tests for $DESC"
+            displaycmd=""
+            if [[ "$1" == "-d" ]]; then
+                displyacmd="-DdisplayCmd=true"
+                shift
+            fi
 
-        mvn $displaycmd -Pitest verify
-        ;;
+            mvn $displaycmd -Pitest verify
+            ;;
 
-    scalatest)
-        shift
-        display=""
-        if [[ "$1" == "" ]]; then
-            display="-eNDXEHLO"
-        fi
+        scalatest)
+            shift
+            display=""
+            if [[ "$1" == "" ]]; then
+                display="-eNDXEHLO"
+            fi
 
-        #See http://www.scalatest.org/user_guide/using_the_runner for command line options
-        #Add -DdisplayCmd=true to see command used...
-        mvn scala:run -Dlauncher=scalatest -DaddArgs="$display"
-        ;;
+            #See http://www.scalatest.org/user_guide/using_the_runner for command line options
+            #Add -DdisplayCmd=true to see command used...
+            mvn scala:run -Dlauncher=scalatest -DaddArgs="$display"
+            ;;
 
-    console)
-        rlwrap mvn scala:console
-        ;;
+        console)
+            rlwrap mvn scala:console
+            ;;
 
-    targz)
-        rm chamber.tar.gz
-        rm target/chamber-0.1-SNAPSHOT-allinone.jar
-        mvn -DskipTests -Pallinone clean package
-        tar -cvzf chamber.tar.gz --exclude chamber.pid --exclude chamber.iml --exclude std.out --exclude out --exclude-vcs --exclude-backups *
-        ;;
+        targz)
+            rm chamber.tar.gz
+            rm target/chamber-0.1-SNAPSHOT-allinone.jar
+            mvn -DskipTests -Pallinone clean package
+            tar -cvzf chamber.tar.gz --exclude chamber.pid --exclude chamber.iml --exclude std.out --exclude out --exclude-vcs --exclude-backups *
+            ;;
 
-    install)
-        sudo ln -s /usr/local/lib/chamber/src/main/webapp current
-	    cd /usr/local/lib/chamber
+        install)
+            sudo ln -s /usr/local/lib/chamber/src/main/webapp current
+            cd /usr/local/lib/chamber
 
-	    sudo apt-get install libssl0.9.8 
-	    sudo apt-get install lynx
-	    sudo apt-get install curl
-	    sudo apt-get install git
+            sudo apt-get install libssl0.9.8 
+            sudo apt-get install lynx
+            sudo apt-get install curl
+            sudo apt-get install git
 
-        sudo apt-get install nginx
-        sudo rm -f /etc/nginx/sites-enabled/default
-        sudo rm -f /etc/nginx/nginx.conf
-        sudo cp src/main/ops/etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default
-        sudo cp src/main/ops/etc/nginx/nginx.conf /etc/nginx/nginx.conf
-        sudo service nginx restart
+            sudo apt-get install nginx
+            sudo rm -f /etc/nginx/sites-enabled/default
+            sudo rm -f /etc/nginx/nginx.conf
+            sudo cp src/main/ops/etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default
+            sudo cp src/main/ops/etc/nginx/nginx.conf /etc/nginx/nginx.conf
+            sudo service nginx restart
 
-        sudo apt-get install mysql-server
-        sudo rm -f /etc/mysql/my.cnf
-        sudo cp src/main/ops/etc/mysql/my.cnf /etc/mysql/my.cnf
-        sudo service mysql restart
+            sudo apt-get install mysql-server
+            sudo rm -f /etc/mysql/my.cnf
+            sudo cp src/main/ops/etc/mysql/my.cnf /etc/mysql/my.cnf
+            sudo service mysql restart
 
-        sudo apt-get install python-pip
-        sudo pip install pagerduty
-        #sudo cp src/main/ops/usr/local/bin/pagerduty* /usr/local/bin/.
+            sudo apt-get install python-pip
+            sudo pip install pagerduty
+            #sudo cp src/main/ops/usr/local/bin/pagerduty* /usr/local/bin/.
 
-        sudo apt-get install monit
-        sudo rm -f /etc/default/monit
-        sudo rm -f /etc/monit/monitrc
-        sudo cp src/main/ops/etc/default/monit /etc/default/monit
-        sudo cp src/main/ops/etc/monit/monitrc /etc/monit/monitrc
-        sudo cp src/main/ops/etc/monit/conf.d/* /etc/monit/conf.d/.
-        sudo service monit restart
+            sudo apt-get install monit
+            sudo rm -f /etc/default/monit
+            sudo rm -f /etc/monit/monitrc
+            sudo cp src/main/ops/etc/default/monit /etc/default/monit
+            sudo cp src/main/ops/etc/monit/monitrc /etc/monit/monitrc
+            sudo cp src/main/ops/etc/monit/conf.d/* /etc/monit/conf.d/.
+            sudo service monit restart
 
-        sudo cp src/main/ops/etc/cloudkick.conf /etc/cloudkick.conf
-	    sudo rm -f /etc/apt/sources.list.d/cloudkick.list
-	    sudo cp src/main/ops/etc/apt/sources.list.d/cloudkick.list /etc/apt/sources.list.d/cloudkick.list
-        sudo curl http://packages.cloudkick.com/cloudkick.packages.key >> cloudkick.packages.key
-        sudo apt-key add cloudkick.packages.key
-        sudo apt-get update
-        sudo apt-get install cloudkick-agent
-        
-        sudo mkdir -p /usr/local/lib/newrelic/logs
-        sudo chmod -R ugo+rwx /usr/local/lib/newrelic
-        #sudo cp src/main/ops/opt/newrelic/newrelic* /opt/newrelic/.
-        ;;
+            sudo cp src/main/ops/etc/cloudkick.conf /etc/cloudkick.conf
+            sudo rm -f /etc/apt/sources.list.d/cloudkick.list
+            sudo cp src/main/ops/etc/apt/sources.list.d/cloudkick.list /etc/apt/sources.list.d/cloudkick.list
+            sudo curl http://packages.cloudkick.com/cloudkick.packages.key >> cloudkick.packages.key
+            sudo apt-key add cloudkick.packages.key
+            sudo apt-get update
+            sudo apt-get install cloudkick-agent
+            
+            sudo mkdir -p /usr/local/lib/newrelic/logs
+            sudo chmod -R ugo+rwx /usr/local/lib/newrelic
+            #sudo cp src/main/ops/opt/newrelic/newrelic* /opt/newrelic/.
+            ;;
 
-    status)
-        ps uh -p `cat chamber.pid`
-        ;;
+        status)
+            ps uh -p `cat chamber.pid`
+            ;;
 
-    compass)
-        compass watch -c src/main/compass/${ENV_TYPE}.rb
-        ;;
+        compass)
+            compass watch -c src/main/compass/${ENV_TYPE}.rb
+            ;;
 
-    compass_compile)
-        compass compile -f -c src/main/compass/${ENV_TYPE}.rb
-        ;;
+        compass_compile)
+            compass compile -f -c src/main/compass/${ENV_TYPE}.rb
+            ;;
 
-    migrate_status)
-        PACKAGE="mvn migration:status -Dmigration.path=src/main/database"
-        echo "Running ${PACKAGE}"
-        $PACKAGE
-        result=$?
-        if [[ $result > 0 ]]; then
-            exit 1;
-        fi
-        ;;
+        migrate_status)
+            PACKAGE="mvn migration:status -Dmigration.path=src/main/database"
+            echo "Running ${PACKAGE}"
+            $PACKAGE
+            result=$?
+            if [[ $result > 0 ]]; then
+                exit 1;
+            fi
+            ;;
 
-    migrate_up)
-        PACKAGE="mvn migration:up -Dmigration.path=src/main/database"
-        echo "Running ${PACKAGE}"
-        $PACKAGE
-        result=$?
-        if [[ $result > 0 ]]; then
-            exit 1;
-        fi
-        ;;
+        migrate_up)
+            PACKAGE="mvn migration:up -Dmigration.path=src/main/database"
+            echo "Running ${PACKAGE}"
+            $PACKAGE
+            result=$?
+            if [[ $result > 0 ]]; then
+                exit 1;
+            fi
+            ;;
 
-    migrate_down)
-        PACKAGE="mvn migration:down -Dmigration.path=src/main/database"
-        echo "Running ${PACKAGE}"
-        $PACKAGE
-        result=$?
-        if [[ $result > 0 ]]; then
-            exit 1;
-        fi
-        ;;
+        migrate_down)
+            PACKAGE="mvn migration:down -Dmigration.path=src/main/database"
+            echo "Running ${PACKAGE}"
+            $PACKAGE
+            result=$?
+            if [[ $result > 0 ]]; then
+                exit 1;
+            fi
+            ;;
 
-    dump)
-        DATE=`date +%Y%m%d%H%M%S`
-        DUMP="mysqldump -u root -ppassword --database Echoed"
-        echo "Running $DUMP | gzip > dump-${DATE}.sql.gz"
-        $DUMP | gzip > dump-${DATE}.sql.gz
-         
-        result=$?
-        if [[ $result > 0 ]]; then
-            exit 1;
-        fi
-        ;;
+        dump)
+            DATE=`date +%Y%m%d%H%M%S`
+            DUMP="mysqldump -u ${DATABASE_USER} -p${DATABASE_PASSWORD} --database ${DATABASE}"
+            echo "Running $DUMP | gzip > dump-${DATABASE}-${DATE}.sql.gz"
+            $DUMP | gzip > dump-${DATABASE}-${DATE}.sql.gz
+             
+            result=$?
+            if [[ $result > 0 ]]; then
+                exit 1;
+            fi
+            ;;
 
-    *)
-        echo "Usage: $NAME {start|stop|restart|reload|status|verify|scalatest|console|targz|package|clean|compass|compass_compile|migrate_status|migrate_up|migrate_down|dump}" >&2
-        exit 1
-        ;;
-esac
+        *)
+            echo "Usage: $NAME {start|stop|restart|reload|status|verify|scalatest|console|targz|package|clean|compass|compass_compile|migrate_status|migrate_up|migrate_down|dump}" >&2
+            exit 1
+            ;;
+    esac
 }
 
 CMD=$*
