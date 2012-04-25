c331d2e61449d9649af0be5b3bf4f68eb2e0ea8a
Jonathan Wu <JonLWu@gmail.com>
2012-04-25 10:59:27 -0400

    Updated jquery.echoed.js to handle different baseURLs

--- src/main/webapp/js/jquery.echoed.js
+++ src/main/webapp/js/jquery.echoed.js
@@ -3,14 +3,21 @@ window.onload = function(){
     loadJSInclude('https://crypto-js.googlecode.com/files/2.5.3-crypto-sha1-hmac-pbkdf2-blockmodes-aes.js',function(){
         loadJSInclude('https://c779203.ssl.cf2.rackcdn.com/rawdeflate.js', function(){
             loadJSInclude('https://c779203.ssl.cf2.rackcdn.com/jquery-1.6.2.min.js', function(){
-                var string = JSON.stringify(echoedRequest);
+                if(EchoedBaseUrl == undefined){
+                    EchoedBaseUrl = "https://www.echoed.com";
+                }
 
+
+
+                var string = JSON.stringify(echoedRequest);
                 var secret = Crypto.util.base64ToBytes(EchoedKey);
                 var iv = Crypto.charenc.Binary.stringToBytes("1234567890123456");
 
                 var content = Crypto.util.bytesToBase64(Crypto.charenc.Binary.stringToBytes(RawDeflate.deflate(string)));
 
                 var crypted = Crypto.AES.encrypt(content ,secret,{ mode: new Crypto.mode.CBC(Crypto.pad.NoPadding), iv: iv });
+
+
                 crypted = crypted.replace(/\+/g,"-");
                 crypted = crypted.replace(/\//g,"_");
                 console.log("Encrypted: " + crypted);
@@ -21,7 +28,7 @@ window.onload = function(){
                 _body.appendChild(script);
                 var requestScript = document.createElement("script");
                 requestScript.type = "text/javascript";
-                requestScript.src = 'https://www.echoed.com/echo/js?pid=' + EchoedPartnerId;
+                requestScript.src = EchoedBaseUrl + '/echo/js?pid=' + EchoedPartnerId;
                 _body.appendChild(requestScript);
             })
         })
