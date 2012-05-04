5769cce6f51d09f9758ce268d890edb933055a94
Jonathan Wu <jwu@echoed.com>
2012-05-03 22:38:08 -0400

    Fixed admin.js

--- src/main/webapp/js/echoed-admin.js
+++ src/main/webapp/js/echoed-admin.js
@@ -127,37 +127,6 @@ Echoed.Views.Pages.Summary = Backbone.View.extend({
         var self = this;
         var template = _.template($('#templates-pages-summary').html());
         self.element.html(template);
-
-Echoed.Router = Backbone.Router.extend({
-    initialize:function (options) {
-        _.bindAll(this, 'summary');
-        this.page = null;
-        this.EvAg = options.EvAg;
-    },
-    routes:{
-        "": "summary",
-        "summary":"summary",
-        "partners":"partners"
-    },
-    summary: function(){
-        var pageView = new Echoed.Views.Pages.Summary({EvAg:this.EvAg});
-        this.page = "summary";
-    },
-    partners: function(){
-        var pageView = new Echoed.Views.Pages.Partners({EvAg:this.EvAg});
-        this.page = "partners";
-    }
-});
-
-Echoed.Views.Pages.Summary = Backbone.View.extend({
-    el:'#content',
-    initialize:function (options) {
-        _.bindAll(this);
-        this.EvAg = options.EvAg;
-        this.render();
-    },
-    render:function () {
-
         $.ajax({
             url:Echoed.urls.api + "/admin/users",
             dataType:'json',
@@ -212,94 +181,10 @@ Echoed.Views.Pages.Summary = Backbone.View.extend({
                 var echoPossibilityTable = new Echoed.Views.Components.TableTemplate({ EvAg:self.EvAg, el:"#echoes", table:table})
             }
         });
-
-Echoed.Router = Backbone.Router.extend({
-    initialize:function (options) {
-        _.bindAll(this, 'summary');
-        this.page = null;
-        this.EvAg = options.EvAg;
-    },
-    routes:{
-        "": "summary",
-        "summary":"summary",
-        "partners":"partners"
-    },
-    summary: function(){
-        var pageView = new Echoed.Views.Pages.Summary({EvAg:this.EvAg});
-        this.page = "summary";
-    },
-    partners: function(){
-        var pageView = new Echoed.Views.Pages.Partners({EvAg:this.EvAg});
-        this.page = "partners";
     }
 });
 
-Echoed.Views.Pages.Summary = Backbone.View.extend({
-    el:'#content',
-    initialize:function (options) {
-        _.bindAll(this);
-        this.EvAg = options.EvAg;
-        this.render();
-    },
-    render:function () {
 
-        $.ajax({
-            url:Echoed.urls.api + "/admin/users",
-            dataType:'json',
-            xhrFields:{
-                withCredentials:true
-            },
-            success:function (users) {
-                var table = {"style":"report-table", "header":[
-                    {"text":"Name"},
-                    {"text":"Created On"},
-                    {"text":"Email"}
-                ], "rows":[]};
-                $.each(users, function (index, user) {
-                    var row = { "href":"#", cells:[]};
-                    var date = new Date(user.createdOn);
-                    row.cells.push({"text":user.name});
-                    row.cells.push({"text":date.toDateString()});
-                    row.cells.push({"text":user.email });
-                    table.rows.push(row);
-                });
-                var userTable = new Echoed.Views.Components.TableTemplate({ EvAg:self.EvAg, el:"#echoed-users", table:table})
-            }
-        });
-        $.ajax({
-            url:Echoed.urls.api + "/admin/echoPossibility",
-            dataType:'json',
-            xhrFields:{
-                withCredentials:true
-            },
-            success:function (echoPossibilities) {
-                var table = {"style":"report-table", "header":[
-                    {"text":"Date"},
-                    {"text":"Partner Id"},
-                    {"text":"Order Id"},
-                    {"text":"Product Id"},
-                    {"text":"Price"},
-                    {"text":"view"},
-                    {"text":"step"}
-                ], rows:[]};
-                $.each(echoPossibilities, function (index, echoPossibility) {
-                    var row = { "href":"#", cells:[] };
-                    var date = new Date(echoPossibility.createdOn);
-                    row.cells.push({"text":date.toDateString()});
-                    row.cells.push({"text":echoPossibility.partnerId});
-                    row.cells.push({"text":echoPossibility.orderId});
-                    row.cells.push({"text":echoPossibility.productId});
-                    row.cells.push({"text":echoPossibility.price});
-                    row.cells.push({"text":echoPossibility.view});
-                    row.cells.push({"text":echoPossibility.step});
-                    table.rows.push(row);
-                });
-                var echoPossibilityTable = new Echoed.Views.Components.TableTemplate({ EvAg:self.EvAg, el:"#echoes", table:table})
-            }
-        });
-
-    }
-});
 
 Echoed.Views.Components.Title = Backbone.View.extend({
     el:'title',
