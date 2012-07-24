1ba36d17d3a2680a4d90a2ac03913777dc81abf5
Matthew Pflueger <matthew.pflueger@gmail.com>
2012-07-24 17:00:31 -0400

    Hopefully the final fix attempt for the Access-Control-Allow headers

--- src/main/scala/com/echoed/chamber/filters/AccessControlHeadersFilter.scala
+++ src/main/scala/com/echoed/chamber/filters/AccessControlHeadersFilter.scala
@@ -9,30 +9,19 @@ import scala.collection.JavaConversions._
 
 class AccessControlHeadersFilter extends Filter {
 
-    private val logger = LoggerFactory.getLogger(classOf[AccessControlHeadersFilter])
-
-    @BeanProperty var domain: String = _
-
     def init(filterConfig: FilterConfig) {}
 
     def doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, chain: FilterChain) {
         val request = servletRequest.asInstanceOf[HttpServletRequest]
         val response = servletResponse.asInstanceOf[HttpServletResponse]
 
-        val origin = Option(request.getHeader("Origin"))
-        if (origin.map(_.endsWith(domain)).getOrElse(false)) {
-            val headers =
-                if (logger.isDebugEnabled) {
-                    request.getHeaderNames.map(h => "\n    %s: %s" format(h, request.getHeader(h))).mkString
-                } else "<empty>"
-            logger.debug("Adding Access-Control-Allow headers for %s" format headers)
-            response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"))
-            response.addHeader("Access-Control-Allow-Methods", "POST, PUT, DELETE, GET, OPTIONS")
-            response.addHeader("Access-Control-Allow-Headers", "Content-Type, X-Scheme, X-Forwarded-For")
-            response.addHeader("Access-Control-Allow-Credentials", "true")
-        } else {
-            logger.debug("Did not add Access-Control-Allow headers: origin {} does not end with {}", origin.orNull, domain)
-        }
+        //leaving this open to allow for our partner widgets to access us easily (no iframes)
+        response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"))
+        response.addHeader("Access-Control-Allow-Methods", "POST, PUT, DELETE, GET, OPTIONS")
+        response.addHeader(
+                "Access-Control-Allow-Headers",
+                "Origin, Host, Allow, Accept, Content-Type, X-Real-IP, X-Scheme, X-Forwarded-For")
+        response.addHeader("Access-Control-Allow-Credentials", "true")
 
         chain.doFilter(request, response)
     }
--- src/main/scala/com/echoed/chamber/filters/CacheControlFilter.scala
+++ src/main/scala/com/echoed/chamber/filters/CacheControlFilter.scala
@@ -7,12 +7,12 @@ import org.slf4j.LoggerFactory
 
 class CacheControlFilter extends Filter {
 
-    private val logger = LoggerFactory.getLogger(classOf[CacheControlFilter])
-
     def init(filterConfig: FilterConfig) {}
 
     def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
         val httpResponse = response.asInstanceOf[HttpServletResponse]
+
+        //anything that needs to be cached should be served from Nginx or the CDN...
         httpResponse.addHeader("Cache-Control", "no-cache")
         httpResponse.addHeader("Expires", "0")
         httpResponse.addHeader("Pragma", "no-cache")
