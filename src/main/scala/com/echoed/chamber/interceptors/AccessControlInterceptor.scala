4300adfbced64f464650ee165d566a955c321ef9
Matthew Pflueger <matthew.pflueger@gmail.com>
2012-07-24 14:54:55 -0400

    Added debug statements for Access-Control-Allow header stuff

--- src/main/scala/com/echoed/chamber/interceptors/AccessControlInterceptor.scala
+++ src/main/scala/com/echoed/chamber/interceptors/AccessControlInterceptor.scala
@@ -4,6 +4,7 @@ import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
 import org.springframework.web.servlet.{ModelAndView, HandlerInterceptor}
 import org.slf4j.LoggerFactory
 import scala.reflect.BeanProperty
+import scala.collection.JavaConversions._
 
 class AccessControlInterceptor extends HandlerInterceptor {
 
@@ -12,11 +13,19 @@ class AccessControlInterceptor extends HandlerInterceptor {
     @BeanProperty var domain: String = _
 
     def preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Object) = {
-        if (Option(request.getHeader("Origin")).map(_.endsWith(domain)).getOrElse(false)) {
+        val origin = Option(request.getHeader("Origin"))
+        if (origin.map(_.endsWith(domain)).getOrElse(false)) {
+            val headers =
+                if (logger.isDebugEnabled) {
+                    request.getHeaderNames.map(h => "    %s: %s\n" format(h, request.getHeader(h)))
+                } else ""
+            logger.debug("Adding Access-Control-Allow headers for\n", headers)
             response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"))
             response.addHeader("Access-Control-Allow-Methods", "*") //POST, PUT, DELETE, GET, OPTIONS")
             response.addHeader("Access-Control-Allow-Headers", "*")
             response.addHeader("Access-Control-Allow-Credentials", "true")
+        } else {
+            logger.debug("Did not add Access-Control-Allow headers: origin {} does not end with {}", origin.orNull, domain)
         }
         true
     }
