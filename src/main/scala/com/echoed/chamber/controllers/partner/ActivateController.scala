f2d066a34895708c688147f7ac5c98696ac7c8ba
Matthew Pflueger <matthew.pflueger@gmail.com>
2012-09-04 17:44:28 -0400

    Fixed registering a new partner
    Worst QA team ever - this thing has been broken for a long time :(

--- src/main/scala/com/echoed/chamber/controllers/partner/ActivateController.scala
+++ src/main/scala/com/echoed/chamber/controllers/partner/ActivateController.scala
@@ -6,7 +6,6 @@ import javax.validation.Valid
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.{PathVariable, RequestMapping, RequestMethod}
 import com.echoed.chamber.services.partneruser._
-import com.echoed.util.ScalaObjectMapper
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.context.request.async.DeferredResult
 import com.echoed.chamber.controllers.{FormController, EchoedController, Errors}
@@ -21,13 +20,17 @@ class ActivateController extends EchoedController with FormController {
     @Autowired var formValidator: ActivateFormValidator = _
 
     @RequestMapping(value = Array("/partner/activate/{code}"), method = Array(RequestMethod.GET))
-    def activateGet(@PathVariable("code") code: String) = {
+    def activateGet(
+            @PathVariable("code") code: String,
+            request: HttpServletRequest,
+            response: HttpServletResponse) = {
         val result = new DeferredResult(new ModelAndView(v.activateView))
 
         log.debug("Starting activation with code {}", code)
 
         mp(LoginWithCode(code)).onSuccess {
             case LoginWithCodeResponse(_, Right(partnerUser)) =>
+                cookieManager.addPartnerUserCookie(response, partnerUser, request)
                 val modelAndView = new ModelAndView(v.activateView)
                 modelAndView.addObject("partnerUser", partnerUser)
                 modelAndView.addObject("activateForm", new ActivateForm(partnerUser.id))
@@ -42,9 +45,7 @@ class ActivateController extends EchoedController with FormController {
     def activatePost(
             @Valid activateForm: ActivateForm,
             bindingResult: BindingResult,
-            pucc: PartnerUserClientCredentials,
-            request: HttpServletRequest,
-            response: HttpServletResponse) = {
+            pucc: PartnerUserClientCredentials) = {
 
         val errorModelAndView = new ModelAndView(v.activateView) with Errors
 
@@ -55,7 +56,7 @@ class ActivateController extends EchoedController with FormController {
         if (bindingResult.hasErrors) {
             errorModelAndView
         } else {
-            val result = new DeferredResult(errorModelAndView)
+            val result = new DeferredResult()
 
             log.debug("Activating partner user {}", activateForm.partnerUserId)
 
@@ -69,10 +70,9 @@ class ActivateController extends EchoedController with FormController {
                         bindingResult.rejectValue("password", e.code.get, e.message)
                         result.set(errorModelAndView)
                     case ActivatePartnerUserResponse(_, Right(partnerUser)) =>
-                        cookieManager.addPartnerUserCookie(response, partnerUser, request)
                         val modelAndView = new ModelAndView(v.postActivateView)
                         modelAndView.addObject("partnerUser", partnerUser)
-                        result.set("modelAndView", modelAndView)
+                        result.set(modelAndView)
                         log.debug("Activated partner user {}: {}", partnerUser.id, partnerUser.name)
                     case ActivatePartnerUserResponse(_, Left(e)) =>
                         errorModelAndView.addError(e)
