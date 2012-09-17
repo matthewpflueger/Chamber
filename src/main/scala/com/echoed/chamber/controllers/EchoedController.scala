aa4c4eaa53ea94db59baab253ffa8c8a120bed15
Matthew Pflueger <matthew.pflueger@gmail.com>
2012-09-17 13:04:43 -0400

    Added partner handle path redirect to #partner/<handle> fragment

--- src/main/scala/com/echoed/chamber/controllers/EchoedController.scala
+++ src/main/scala/com/echoed/chamber/controllers/EchoedController.scala
@@ -3,6 +3,8 @@ package com.echoed.chamber.controllers
 import com.echoed.util.{Encrypter, Logging}
 import com.echoed.chamber.services.{EventProcessor, MessageProcessor}
 import org.springframework.beans.factory.annotation.Autowired
+import java.util.Properties
+import javax.annotation.Resource
 
 abstract class EchoedController {
     protected val log = Logging(this.getClass)
@@ -12,4 +14,8 @@ abstract class EchoedController {
     @Autowired protected var ep: EventProcessor = _
     @Autowired protected var cookieManager: CookieManager = _
     @Autowired protected var encrypter: Encrypter = _
+
+    @Resource(name = "urlsProperties") private var urlsProperties: Properties = _
+
+    protected def getHttpSiteUrl = urlsProperties.getProperty("http.urls.site")
 }
