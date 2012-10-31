12fdd923e59b18b0fd0fd0feec351940cbc5d4ba
MisterJwu <JonLWu@gmail.com>
2012-10-31 17:38:55 -0400

    Removed devault jquerymobile loading

--- src/main/webapp/scripts/libs/jquery.mobile.config.js
+++ src/main/webapp/scripts/libs/jquery.mobile.config.js
@@ -4,6 +4,7 @@ define(['jquery'], function ($) {
         $.mobile.linkBindingEnabled = false;
         $.mobile.hashListeningEnabled = false;
         $.mobile.pushStateEnabled = false;
+        $.mobile.loadingMessage = false;
         $.event.special.swipe.scrollSupressionThreshold = 100;
         $.event.special.swipe.durationThreshold = 1000;
         $.event.special.swipe.horizontalDistanceThreshold = 175;
