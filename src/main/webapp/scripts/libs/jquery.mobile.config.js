49d9b26e55472e846410af71dbe2e03a9c43e0dc
MisterJwu <JonLWu@gmail.com>
2012-11-01 12:59:21 -0400

    Removed duration configuration

--- src/main/webapp/scripts/libs/jquery.mobile.config.js
+++ src/main/webapp/scripts/libs/jquery.mobile.config.js
@@ -6,7 +6,7 @@ define(['jquery'], function ($) {
         $.mobile.pushStateEnabled = false;
         $.mobile.loadingMessage = false;
         $.event.special.swipe.scrollSupressionThreshold = 100;
-        $.event.special.swipe.durationThreshold = 1000;
+        //$.event.special.swipe.durationThreshold = 1000;
         $.event.special.swipe.horizontalDistanceThreshold = 80;
         //$.event.special.swipe.verticalDistanceThreshold = 30;
     });
