94b3b616e06ff66f9fcd99cbc2baf019b3274894
MisterJwu <JonLWu@gmail.com>
2012-11-19 21:16:11 -0500

    Updated title bar to prototype topics

--- src/main/webapp/scripts/components/title.js
+++ src/main/webapp/scripts/components/title.js
@@ -14,20 +14,11 @@ define(
                 this.titleShareLabel= $('#title-share-label');
             },
             update: function(options){
-                if(options.href !== ""){
-                    this.titleActions.show();
-                } else{
-                    this.titleActions.hide();
-                }
-                if(options.title !== ""){
-                    this.titleText.text(decodeURIComponent(options.title));
-                    this.titleShareLabel.text("Share Your " + options.title + " Story");
-                    this.titleShare.attr("href", options.href);
-                    this.element.show()
-                } else{
-                    this.titleText.text("");
-                    this.element.hide();
-                }
+                //this.titleText.text(decodeURIComponent(options.title));
+                this.titleShareLabel.text("Share Your " + options.title + " Story");
+                this.titleShare.attr("href", options.href);
+                this.element.show();
+
             }
         });
     }
