97d53c52ce8e176518daf61b8343f84ef4b394ab
Matthew Pflueger <matthew.pflueger@gmail.com>
2012-12-04 12:55:55 -0500

    Now only need to specify paths in one place

---
+++ src/main/webapp/scripts/app.config.js
@@ -0,0 +1,23 @@
+require.config({
+    paths: {
+        'mobileConfig'      : 'jquery.mobile.config',
+
+        'backbone'          : 'libs/backbone-0.9.2',
+        'easyXDM'           : 'libs/easyXDM-2.4.16.3',
+        'json2'             : 'libs/json2-20111019',
+        'underscore'        : 'libs/underscore-1.4.1',
+
+        'expanding'         : 'libs/jquery/expanding-20121116',
+        'cloudinary'        : 'libs/jquery/jquery.cloudinary-1.0.1',
+        'fileUploader'      : 'libs/jquery/jquery.fileupload-5.10.1',
+        'iFrameTransport'   : 'libs/jquery/jquery.iframe-transport-1.4',
+        'isotope'           : 'libs/jquery/jquery.isotope-1.5.21',
+        'jqueryMobile'      : 'libs/jquery/jquery.mobile-1.2.0',
+        'jquery'            : 'libs/jquery/jquery-1.8.3',
+        'jqueryUI'          : 'libs/jquery/jquery-ui-1.9.1.custom',
+
+        'requireLib'        : 'libs/require/require-2.1.1',
+        'text'              : 'libs/require/text-2.0.3'
+    }
+});
+
