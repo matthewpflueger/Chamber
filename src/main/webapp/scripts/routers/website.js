e1518a9bebdcaf564f51f7fd0f27e7cf3ca116bc
MisterJwu <JonLWu@gmail.com>
2013-01-29 11:12:50 -0500

    Fixed closing of windows / popups to reset hash correctly

--- src/main/webapp/scripts/routers/website.js
+++ src/main/webapp/scripts/routers/website.js
@@ -22,10 +22,10 @@ define(
             },
             routes:{
                 "_=_":                      "fix",
-                "!":                        "explore",
-                "":                         "explore",
-                "!me/":                     "me",
-                "!me":                      "me",
+                "!":                        "feed",
+                "":                         "feed",
+                "!me/":                     "feed",
+                "!me":                      "feed",
                 "!story/:id":               "story",
                 "story/:id":                "story",
                 "!photo/:id":               "photo",
@@ -46,15 +46,18 @@ define(
                 window.location.href = "#";
             },
             content: function(context, id, type, type2){
-                this.page = window.location.hash;
-                var self = this;
-                var url = context + "/" + id;
+                var self =      this;
+                var url =       context + "/" + id;
                 if(context === "user" && this.modelUser.is(id)) url = "/me";
                 if(type) url += "/" + type;
                 if(type2) url += "/" + type2;
-                this.requestFeed(url, function(jsonUrl, data){
-                    self.loadPage(context, { jsonUrl: jsonUrl, data: data, personal: true} );
-                });
+                var page = url;
+                if(this.page !== page){
+                    this.page = page;
+                    this.requestFeed(url, function(jsonUrl, data){
+                        self.loadPage(context, { jsonUrl: jsonUrl, data: data, personal: true} );
+                    });
+                }
             },
             requestFeed: function(endPoint, callback){
                 var self = this;
@@ -77,23 +80,28 @@ define(
             },
             feed: function(type){
                 var self = this;
-                if(this.page != window.location.hash){
-                    this.page = window.location.hash;
-                    var url = "/me/feed";
+                if(!this.modelUser.isLoggedIn()){
+                    console.log("Explore");
+                    this.explore(type);
+                } else {
+                    var url = "me/feed";
                     if(type) url += "/" + type;
-                    this.requestFeed(url, function(jsonUrl, data){
-                        self.loadPage("explore", { jsonUrl: jsonUrl, data: data });
-                    });
+                    if(this.page != url){
+                        this.page = url;
+                        this.requestFeed(url, function(jsonUrl, data){
+                            self.loadPage("explore", { jsonUrl: jsonUrl, data: data });
+                        });
+                    }
                 }
             },
-            explore: function(){
+            explore: function(type){
                 var self = this;
-                if(this.page != window.location.hash){
-                    this.page = "#!";
-                    var url = "/me/feed";
-                    if(!this.modelUser.isLoggedIn()) url = "/public/feed";
+                var url = "public/feed"
+                if(type) url += "/" + type;
+                if(this.page != url){
+                    this.page = url
                     this.requestFeed(url, function(jsonUrl, data){
-                    self.loadPage("explore", { jsonUrl: jsonUrl, data: data });
+                        self.loadPage("explore", { jsonUrl: jsonUrl, data: data });
                     });
                 }
             },
@@ -101,10 +109,8 @@ define(
                 if(this.page === null){
                     if(this.modelUser.isLoggedIn()){
                         this.content();
-                        this.page = "#!me";
                     } else {
                         this.explore();
-                        this.page = "#";
                     }
                 }
                 this.oldPage = this.page;
@@ -122,7 +128,6 @@ define(
             story: function(id){
                 if(this.page === null) {
                     this.explore();
-                    this.page = "#!";
                 }
                 this.oldPage = this.page;
                 this.oldTitle = $('title').html();
