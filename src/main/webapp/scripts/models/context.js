3c07d3399253a071b22c21c683ee1a2f4ceb8bf0
MisterJwu <JonLWu@gmail.com>
2013-02-13 17:15:05 -0500

    Fixed 'me' in context'

--- src/main/webapp/scripts/models/context.js
+++ src/main/webapp/scripts/models/context.js
@@ -6,7 +6,11 @@ define(
         return Backbone.Model.extend({
             baseUrl: function(){
                 if(this.has("contextType") && this.has("id")){
-                    return this.get("contextType").toLowerCase() + "/" + this.id + "/";
+                    if(this.get("contextType") === "me"){
+                        return this.get("contextType").toLowerCase() + "/"
+                    } else {
+                        return this.get("contextType").toLowerCase() + "/" + this.id + "/";
+                    }
                 } else {
                     return false;
                 }
